abbf10a360f0cf0c89fe2d1b438080ef907e0171
haidinhthanh <haidt.221296.hust@gmail.com>
2020-01-01 16:53:10 +0700

    hh

--- TalentDataCrawl/data_pipeline/ProcessorPipeline1.py
+++ TalentDataCrawl/data_pipeline/ProcessorPipeline1.py
@@ -13,6 +13,7 @@ from talent_crawl_data.utils.TimeUtils import get_instance_time_iso_format
 from comon.constant import local_elastic, server_elastic
 import time
 import gc
+import tensorflow as tf
 
 
 class ProcessorPipeline1:
@@ -84,65 +85,67 @@ def process_news(item, pipeline):
 
 if __name__ == "__main__":
     # no_crawled_news = ElasticSearchUtils.getNumOfCrawledNewsInOneDay(local_elastic(), "talent-crawled")
-    no_crawled_news = ElasticSearchUtils.getNODoc(local_elastic(), "talent-crawled")
-    print(no_crawled_news)
-    from_new_index = 0
-    size_new_index = 100
+    # no_crawled_news = ElasticSearchUtils.getNODoc(local_elastic(), "talent-crawled")
+    # print(no_crawled_news)
+    # from_new_index = 0
+    # size_new_index = 100
+    #
+    # crawled_news = []
+    # while True:
+    #     if from_new_index > no_crawled_news:
+    #         break
+    #     crawled_new = ElasticSearchUtils.getCrawledNewsFormHostInOneDay(local_elastic(), "talent-crawled",
+    #                                                                     from_new_index, size_new_index)
+    #     if not crawled_new:
+    #         break
+    #     crawled_news += crawled_new
+    #     from_new_index += size_new_index
+    # # run through processor
+    # crawled_source_news = [item["_source"] for item in crawled_news]
+    # i = 0
+    # try:
+    #     while True:
+    #         pipeline = ProcessorPipeline1()
+    #         processed_items = []
+    #         if i > len(crawled_source_news):
+    #             for item in crawled_source_news[i-10:len(crawled_source_news)]:
+    #                 print("Running: " + str(crawled_source_news.index(item)))
+    #                 new_item = process_news(item, pipeline)
+    #                 processed_items.append(item)
+    #             print("size processed data", len(processed_items))
+    #             clean_news = [item for item in processed_items
+    #                           if ("processor_irrelevant" in item and "processor_duplicate" in item
+    #                               and item["processor_irrelevant"] == "related"
+    #                               and item["processor_duplicate"] == "not duplicate")]
+    #             print("size clean data: ", len(clean_news))
+    #             print('Pipeline stats:\n', json.dumps(pipeline.stats(), indent=2))
+    #             print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    #             del pipeline
+    #             del clean_news
+    #             del processed_items
+    #             del new_item
+    #             gc.collect()
+    #             break
+    #         else:
+    #             for item in crawled_source_news[i:i+10]:
+    #                 print("Running: " + str(crawled_source_news.index(item)))
+    #                 new_item = process_news(item, pipeline)
+    #                 processed_items.append(new_item)
+    #             print("size processed data", len(processed_items))
+    #             clean_news = [item for item in processed_items
+    #                           if ("processor_irrelevant" in item and "processor_duplicate" in item
+    #                               and item["processor_irrelevant"] == "related"
+    #                               and item["processor_duplicate"] == "not duplicate")]
+    #             print("size clean data: ", len(clean_news))
+    #             print('Pipeline stats:\n', json.dumps(pipeline.stats(), indent=2))
+    #             print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    #             del pipeline
+    #             del clean_news
+    #             del processed_items
+    #             del new_item
+    #             gc.collect()
+    #             i += 10
+    # except MemoryError:
+    #     print("Rerun")
 
-    crawled_news = []
-    while True:
-        if from_new_index > no_crawled_news:
-            break
-        crawled_new = ElasticSearchUtils.getCrawledNewsFormHostInOneDay(local_elastic(), "talent-crawled",
-                                                                        from_new_index, size_new_index)
-        if not crawled_new:
-            break
-        crawled_news += crawled_new
-        from_new_index += size_new_index
-    # run through processor
-    crawled_source_news = [item["_source"] for item in crawled_news]
-    i = 0
-    try:
-        while True:
-            pipeline = ProcessorPipeline1()
-            processed_items = []
-            if i > len(crawled_source_news):
-                for item in crawled_source_news[i-10:len(crawled_source_news)]:
-                    print("Running: " + str(crawled_source_news.index(item)))
-                    new_item = process_news(item, pipeline)
-                    processed_items.append(item)
-                print("size processed data", len(processed_items))
-                clean_news = [item for item in processed_items
-                              if ("processor_irrelevant" in item and "processor_duplicate" in item
-                                  and item["processor_irrelevant"] == "related"
-                                  and item["processor_duplicate"] == "not duplicate")]
-                print("size clean data: ", len(clean_news))
-                print('Pipeline stats:\n', json.dumps(pipeline.stats(), indent=2))
-                print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-                del pipeline
-                del clean_news
-                del processed_items
-                del new_item
-                gc.collect()
-                break
-            else:
-                for item in crawled_source_news[i:i+10]:
-                    print("Running: " + str(crawled_source_news.index(item)))
-                    new_item = process_news(item, pipeline)
-                    processed_items.append(new_item)
-                print("size processed data", len(processed_items))
-                clean_news = [item for item in processed_items
-                              if ("processor_irrelevant" in item and "processor_duplicate" in item
-                                  and item["processor_irrelevant"] == "related"
-                                  and item["processor_duplicate"] == "not duplicate")]
-                print("size clean data: ", len(clean_news))
-                print('Pipeline stats:\n', json.dumps(pipeline.stats(), indent=2))
-                print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-                del pipeline
-                del clean_news
-                del processed_items
-                del new_item
-                gc.collect()
-                i += 10
-    except MemoryError:
-        print("Rerun")
+    print(tf.__version__)
