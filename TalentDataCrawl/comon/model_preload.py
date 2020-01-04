2b4ae9044afd67d2699563f8d4eca13d07821b7a
haidinhthanh <haidt.221296.hust@gmail.com>
2020-01-04 16:25:26 +0700

    h

--- TalentDataCrawl/comon/model_preload.py
+++ TalentDataCrawl/comon/model_preload.py
@@ -1,7 +1,7 @@
 import os
 import pickle
-dir_path = os.path.dirname(os.path.realpath(__file__))
-path = dir_path.replace("comon", "")
+from const_path import root_path
+path = root_path
 bi_rnn = pickle.load(open(os.path.join(path, "model/bi_rnn_model.pkl"), "rb"))
 irr_naive_bayes = pickle.load(open(os.path.join(path, "model/NB_relevant/naive_bayes_model.pkl"), "rb"))
 cate_svm = pickle.load(open(os.path.join(path, "model/svm_model.pkl"), "rb"))
