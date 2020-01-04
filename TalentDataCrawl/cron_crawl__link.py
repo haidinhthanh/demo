b24f53725fac1957dd3585c43267729414489198
haidinhthanh <haidt.221296.hust@gmail.com>
2020-01-04 21:58:56 +0700

    h

--- TalentDataCrawl/cron_crawl__link.py
+++ TalentDataCrawl/cron_crawl__link.py
@@ -2,7 +2,7 @@ from crontab import CronTab
 import os
 cron = CronTab(user='haidt')
 root_path = os.path.dirname(os.path.realpath(__file__))
-command = "/usr/bin/python3" + os.path.join(root_path, "printfile.py")+ " > /home/my_output.txt"
+command = "/usr/bin/python3 " + os.path.join(root_path, "printfile.py")+ " > /home/my_output.txt"
 job1 = cron.new(command=command)
 
 job1.minute.every(1)
